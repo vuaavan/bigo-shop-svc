6b697204842cdadd6bcef2563b9cd452bfeec849
catdobyte <nghiamd.dev@scuti.asia>
2024-08-06 10:08:30 +0700

    add morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const morgan = require("morgan")
 // const bodyParser = require('body-parser');
 const { sequelize } = require("./models")
 const dishRoutes = require("./routes/dishRoutes")
@@ -6,6 +7,7 @@ const dishRoutes = require("./routes/dishRoutes")
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(morgan("dev"))
 app.use(express.json())
 app.use("/api", dishRoutes)
 
